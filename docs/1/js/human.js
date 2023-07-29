aa90f726fb5412a7c5a4ad4b3367a63ba9bec04c
ytyaru <yttry0@gmail.com>
2023-07-29 18:01:28 +0900

    1 html表示

---
+++ docs/1/js/human.js
@@ -0,0 +1,10 @@
+class Human {
+    constructor(name, life, power, toughness, agility) {
+        this.name = name || ''
+        this.life = life || 0
+        this.power = power || 0
+        this.toughness = toughness || 0
+        this.agility = agility || 0
+    }
+}
+
